ad53214875c9d2eef61736e4de7469c95b2eef75
AnttonK <antton.koivisto@gmail.com>
2023-01-27 11:55:32 +0200

    router toimimaan

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { HashRouter } from 'react-router-dom';
 
 import App from './App'
+
+ReactDOM.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
